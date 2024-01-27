f8c0eebe63e6d4a1a1375a8a530b249d16332f7d
MADHU <madhu@madhus-MacBook-Pro.local>
2024-01-27 13:21:06 +0530

    first commit

---
+++ assets/vendor/php-email-form/validate.js
@@ -0,0 +1,64 @@
+/**
+* PHP Email Form Validation - v3.6
+* URL: https://bootstrapmade.com/php-email-form/
+* Author: BootstrapMade.com
+*/
+(function () {
+  "use strict";
+
+  let forms = document.querySelectorAll('.php-email-form');
+
+  forms.forEach(function (e) {
+    e.addEventListener('submit', function (event) {
+      event.preventDefault();
+
+      let thisForm = this;
+
+      let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
+
+      thisForm.querySelector('.loading').classList.add('d-block');
+      thisForm.querySelector('.error-message').classList.remove('d-block');
+      thisForm.querySelector('.sent-message').classList.remove('d-block');
+
+      let formData = new FormData(thisForm);
+
+      if (false) {
+        if (typeof grecaptcha !== "undefined") {
+          grecaptcha.ready(function () {
+            try {
+              grecaptcha.execute(recaptcha, { action: 'php_email_form_submit' })
+                .then(token => {
+                  formData.set('recaptcha-response', token);
+                  php_email_form_submit(thisForm, action, formData);
+                })
+            } catch (error) {
+              displayError(thisForm, error);
+            }
+          });
+        } else {
+          displayError(thisForm, 'The reCaptcha javascript API url is not loaded!')
+        }
+      } else {
+        php_email_form_submit(thisForm, "", formData);
+      }
+    });
+  });
+
+  function php_email_form_submit(thisForm, action, formData) {
+    const scriptURL = 'https://script.google.com/macros/s/1jGGzf14eDC8zQAMC5UWDMl8y0pU2p7KegbE0qEE3NHRiR9kKIUsVbpUq/exec'
+    fetch(scriptURL, { method: 'POST', body: new FormData(thisForm) })
+      .then(response => {
+        thisForm.querySelector('.loading').classList.remove('d-block');
+        thisForm.querySelector('.sent-message').classList.add('d-block');
+        thisForm.reset();
+      })
+      .catch(error => displayError(thisForm, error.message))
+  }
+
+  function displayError(thisForm, error) {
+    thisForm.querySelector('.loading').classList.remove('d-block');
+    thisForm.querySelector('.error-message').innerHTML = error;
+    thisForm.querySelector('.error-message').classList.add('d-block');
+  }
+
+})();
